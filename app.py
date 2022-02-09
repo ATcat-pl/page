367bddba5684d23b350fc011da27966ca8265d36
ATcat-pl <antektyczka@gmail.com>
2022-02-09 20:50:10 +0100

    log_

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import datetime
+
 import requests
 import os
 from datetime import date
@@ -8,57 +10,63 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render("home.html")
+    return render("home.html", "/")
 
 
 @app.route('/pl/')
 def pl_home_page():
+    log('pl/home.html', '/pl/')
     return render_template('pl/home.html')
 
 
 @app.route('/en/')
 def en_home_page():
+    log('en/home.html', '/en/')
     return render_template('en/home.html')
 
 
 @app.route('/2s1b')
 def MCserwer_page():
+    log('pl/2s1b.html', '/2s1b')
     return render_template('pl/2s1b.html')
 
 
 @app.route('/about_us')
 def about_us_page():
-    return render('about_us_page.html')
+    return render('about_us_page.html', '/about_us')
 
 
 @app.route('/help')
 def help_page():
-    return render('help.html')
+    return render('help.html', '/help')
 
 
 @app.route('/my_yt_page')
 def my_yt_page():
+    log('pl/mój_yt_1.html', '/my_yt_page')
     return render_template('pl/mój_yt_1.html')
 
 
 @app.route('/gra_win98')
 def gra_win98():
+    log('pl/gra_win98.html', '/gra_win98')
     return render_template('pl/gra_win98.html')
 
 
 @app.route('/program_downloads')
 def program_downloads():
-    return render('program_downloads.html')
+    return render('program_downloads.html', '/program_downloads')
 
 
 @app.route('/program_downloads/gra_miasta')
 def program_downloads_gra_miasta():
-    return render("gra_miasta.html")
+    return render("gra_miasta.html", '/program_downloads/gra_miasta')
 
 
 # tymczas
 @app.route('/program_downloads/gra_miasta/test')
 def program_downloads_gra_miasta_test():
+    log('pl/gra_miasta.html', '/program_downloads/gra_miasta/test')
     return render_template('pl/gra_miasta.html')
 
 
@@ -66,48 +74,53 @@ def program_downloads_gra_miasta_test():
 
 @app.route('/program_downloads/gra_miasta/lastest_version')
 def gra_miasta_version():
+    log("Null", '/program_downloads/gra_miasta/lastest_version')
     response = requests.get("https://api.github.com/repos/Miasta-creators/Miasta_gra/releases/latest")
     return response.json()["name"]
 
 
 @app.route('/program_downloads/gra_miasta/update_info')
 def gra_miasta_update_info():
+    log("Null", '/program_downloads/gra_miasta/update_info')
     return "texture+jar"  # texture+jar
 
 
 @app.route('/atcatos')
 def ATcatOS():
+    log('pl/atcat_os_home.html', '/atcatos')
     return render_template('pl/atcat_os_home.html')
 
 
 @app.route('/electronics/boards/ports-to-data-bus-series')
 def PrtToDataBusBoardsSeries1():
+    log("Null", '/electronics/boards/ports-to-data-bus-series')
     return render_template('unfinished.html')
 
 
-def render(name):
+def render(name, path):
     lang = str(request.accept_languages)
     language = lang.split(",")
     if (language[0] == "pl"):
-        log("pl/" + name)
+        log("pl/" + name, path)
         return render_template("pl/" + name)
     else:
-        log("en/" + name)
+        log("en/" + name, path)
         return render_template("en/" + name)
 
-def log(name):
+
+def log(name, path):
     today = date.today()
     if os.path.isfile("log/" + today.strftime("%d-%m-%Y") + ".txt"):
         f = open("log/" + today.strftime("%d-%m-%Y") + ".txt", 'a')
     else:
         f = open("log/" + today.strftime("%d-%m-%Y") + ".txt", 'x')
-    f.write("{\n")
-    f.write("request: " + name + "\n")
-    f.write("IP: " + str(request.remote_addr) + "\n")
+    f.write("{\n" + "request: " + path + ", file: " + name + "\n" + "IP: " + str(request.remote_addr) + "\n" + "User-Agent: " + str(request.headers.get('User-Agent')) + "\n" + "Languages: " + str(request.accept_languages) + "\n" + "}\n")
     print(request.remote_addr)
-    f.write("User-Agent: " + str(request.headers.get('User-Agent')) + "\n")
     print(request.headers.get('User-Agent'))
-    f.write("Languages: " + str(request.accept_languages) + "\n")
     print(request.accept_languages)
-    f.write("}\n")
     f.close()
+
+    old = today - datetime.timedelta(days=10)
+    if os.path.isfile("log/"+old.strftime("%d-%m-%Y")+".txt"):
+        os.remove("log/"+old.strftime("%d-%m-%Y")+".txt")
+        print("log/"+old.strftime("%d-%m-%Y")+".txt")
